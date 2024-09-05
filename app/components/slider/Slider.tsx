1b237f06ccfda609e8ced3e25c94d9877ae1c5fe
emmanuel <emmanuelochigbo60@gmail.com>
2024-09-05 19:59:01 +0100

    add

--- app/components/slider/Slider.tsx
+++ app/components/slider/Slider.tsx
@@ -33,8 +33,8 @@ const Slideshow = () => {
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.8 }}
-          transition={{ duration: 0.5 }}
-          className="max-w-[150px] max-h-[150px] object-cover"
+          transition={{ duration: 2 }} // Adjusted duration for slower effect
+          className="max-w-[150px] max-h-[150px] object-contain"
         />
       </AnimatePresence>
     </div>
