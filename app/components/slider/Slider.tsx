19d1b9b45e1a78cf44f1182dab7875d45c5ca00b
emmanuel <emmanuelochigbo60@gmail.com>
2024-09-05 19:54:19 +0100

    new test

---
+++ app/components/slider/Slider.tsx
@@ -0,0 +1,44 @@
+// components/Slideshow.tsx
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
+
+const images = [
+  "/animation/a-1.svg",
+  "/animation/a-2.svg",
+  "/animation/a-3.svg",
+  "/animation/a-4.svg",
+];
+
+const Slideshow = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    // Set an interval to change the image every 4 seconds
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 4000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative w-full h-[60px] md:h-[80px] flex items-center justify-center overflow-hidden">
+      <AnimatePresence>
+        <motion.img
+          key={images[index]}
+          src={images[index]}
+          alt={`Slide ${index + 1}`}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.8 }}
+          transition={{ duration: 0.5 }}
+          className="max-w-[150px] max-h-[150px] object-cover"
+        />
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Slideshow;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,17 +9,27 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        general: "#ffffff",      
-        primary_green: "#02981D", 
-        primary_black: "#171717", 
-        primary_gray: "#5E5E5E",  
-        secondary_green: "#013D0C", 
-        bold_green: "#015B11",    
+        general: "#ffffff",
+        primary_green: "#02981D",
+        primary_black: "#171717",
+        primary_gray: "#5E5E5E",
+        secondary_green: "#013D0C",
+        bold_green: "#015B11",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        slideshow: "slideshow 9s infinite", // 9s for three images, 3s each
+      },
+      keyframes: {
+        slideshow: {
+          "0%, 33.333%": { opacity: "1" }, // Wrap opacity values in quotes
+          "33.334%, 66.666%": { opacity: "0" },
+          "66.667%, 100%": { opacity: "1" },
+        },
       },
     },
   },
