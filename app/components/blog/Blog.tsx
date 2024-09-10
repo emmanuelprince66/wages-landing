94b3c1360adc5928b0f97eae5e61b33f26fa273d
emmanuel <emmanuelochigbo60@gmail.com>
2024-09-10 13:14:31 +0100

    new chag

--- app/components/blog/Blog.tsx
+++ app/components/blog/Blog.tsx
@@ -30,7 +30,7 @@ const Blog = () => {
   };
 
   return (
-    <div className="flex_column_center text-center w-[90%] md:w-[75%] py-4 mx-auto  gap-4 mt-[10%] md:mt-[3%]  justify-center">
+    <div className="flex_column_center text-center w-[90%]  md:w-[75%] py-4 mx-auto  gap-4 mt-[10%] md:mt-[3%]  justify-center">
       <motion.div
         ref={blogRef}
         initial="hidden"
@@ -50,16 +50,15 @@ const Blog = () => {
         </p>
       </motion.div>
 
-      <div className="w-full mt-[4%]">
+      <div className="w-full mt-[10%] md:mt-[4%]">
         <motion.div
           ref={financialRef}
           initial="hidden"
           animate={financialRefView ? "visible" : "hidden"}
           variants={fadeVariant}
-          className="flex flex-col md:flex-row md:gap-4"
+          className="flex flex-col md:flex-row md:gap-4 w-full"
         >
-          <div></div>
-          <div className="flex flex-col md:flex-row md:flex-1 items-center justify-center md:gap-4">
+          <div className="flex flex-col md:flex-row md:flex-1 items-center justify-between ">
             <div className="order-1 md:order-1 md:flex-1">
               <div className="flex_column gap-5 mb-7 md:mb-0">
                 <p className="  text-[17px] md:text-[20px] font-[600] text-primary_black">
@@ -72,7 +71,7 @@ const Blog = () => {
                 </p>
               </div>
             </div>
-            <div className="order-2 md:order-2 md:flex-1">
+            <div className="order-2 md:order-2 md:flex-2">
               <div className="h-full w-full">
                 <Image
                   className="object-contain pt-1"
@@ -92,9 +91,9 @@ const Blog = () => {
           initial="hidden"
           animate={literacyRefView ? "visible" : "hidden"}
           variants={fadeVariant}
-          className="flex flex-col md:flex-row md:gap-4 mt-[6%]"
+          className="flex flex-col md:flex-row md:gap-4 mt-[10%]"
         >
-          <div className="flex flex-col md:flex-row md:flex-1 items-center justify-center  md:gap-4">
+          <div className="flex flex-col md:flex-row md:flex-1 items-center justify-between ">
             <div className="order-2 md:order-1 md:flex-1">
               <div className="h-full w-full">
                 <Image
@@ -107,23 +106,23 @@ const Blog = () => {
                 />
               </div>
             </div>
-            <div className="order-1 md:order-2 md:flex-1">
-              <div className="flex_column gap-5 mb-7 mt-9 md:mb-0">
+            <div className="order-1 md:order-2 md:flex-1 ">
+              <div className="flex_column gap-5 mb-7 mt-9 md:mb-0 ">
                 <p className="  text-[17px] md:text-[20px] font-[600] text-primary_black">
                   Why You should Be Financial Literate👀
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   1. Being financially literate helps you make informed
                   decisions about saving, investing, and spending.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   2. It allows you to understand the risks and rewards
                   associated with different financial decisions With better
                   financial literacy, you can manage your finances more
                   effectively, avoid unnecessary debt, and plan for future
                   financial needs.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   3. Understanding financial principles helps you set realistic
                   financial goals and create a plan to achieve them.
                 </p>
@@ -137,10 +136,10 @@ const Blog = () => {
           initial="hidden"
           animate={budgetRefView ? "visible" : "hidden"}
           variants={fadeVariant}
-          className="flex flex-col md:flex-row md:gap-4 mt-[6%]"
+          className="flex flex-col md:flex-row md:gap-4 mt-[10%]"
         >
           <div className="flex flex-col md:flex-row md:flex-1 items-center justify-center  md:gap-4">
-            <div className="order-2 md:order-2 md:flex-1">
+            <div className="order-2 md:order-2 md:flex-2">
               <div className="h-full w-full">
                 <Image
                   className="object-contain pt-1"
@@ -157,30 +156,30 @@ const Blog = () => {
                 <p className="  text-[17px] md:text-[20px] font-[600] text-primary_black">
                   Key Components of Financial Literacy📌
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[90%]">
                   <span className="font-[500]"> 1. Budgeting</span>: Learn to
                   create and maintain a budget that tracks your income and
                   expenses.A good budget helps you live within your means.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[90%]">
                   <span className="font-[500]">2. Saving</span>: Discover the
                   importance of saving and different strategies to build your
                   savings.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[90%]">
                   <span className="font-[500]">3. Investing</span>: Gain
                   insights into various investment options and how they can help
                   grow your wealth. Learn about the stock market, bonds, mutual
                   funds, and other investment vehicles, and understand the
                   concept of risk versus reward.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[90%]">
                   <span className="font-[500]"> 4. Debt Management</span>:
                   Understand how to manage debt effectively. Learn about
                   different types of debt, strategies for paying off debt, and
                   how to avoid falling into debt traps.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[90%]">
                   <span className="font-[500]">5. Financial Planning</span>:
                   Explore the importance of financial planning and how to create
                   a comprehensive financial plan. This includes setting
@@ -196,7 +195,7 @@ const Blog = () => {
           initial="hidden"
           animate={helpRefView ? "visible" : "hidden"}
           variants={fadeVariant}
-          className="flex flex-col md:flex-row md:gap-4 mt-[6%]"
+          className="flex flex-col md:flex-row md:gap-4 mt-[10%]"
         >
           <div className="flex flex-col md:flex-row md:flex-1 items-center justify-center  md:gap-4">
             <div className="order-2 md:order-1 md:flex-1">
@@ -216,33 +215,33 @@ const Blog = () => {
                 <p className="  text-[17px] md:text-[20px] font-[600] text-primary_black">
                   How WagesFinance Can Help😌
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   At WagesFinance, we are committed to helping you enhance your
                   financial literacy and achieve your financial goals. Our
                   cooperative system offers a range of tools and resources
                   designed to support you on your financial journey:
                 </p>
-                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   <span className="font-[500]">. Savings Tools:</span> Our app
                   provides personalized savings plans and automated savings
                   features to help you save more effectively.
                 </p>
-                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   <span className="font-[500]">. Investment Guidance:</span>{" "}
                   Access expert investment advice and tools to make informed
                   investment decisions that align with your goals.
                 </p>
-                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   <span className="font-[500]">. Goal Tracking:</span>Set and
                   track your financial goals with our intuitive goal-setting
                   features, ensuring you stay on the path to success.
                 </p>
-                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className=" text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   <span className="font-[500]">. Educational Resources:</span>{" "}
                   Benefit from a wealth of educational materials, including
                   articles, tutorials, and webinars on various financial topics.
                 </p>
-                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 md:max-w-[80%]">
+                <p className="text-[15px] font-[400] text-primary_gray text-start leading-5 ">
                   Financial literacy is not just about understanding money; it's
                   about making your money work for you. With WagesFinance, you
                   have taken the first step towards a secure and prosperous
--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -18,8 +18,8 @@ const Footer = () => {
   return (
     <>
       {!state && (
-        <div className="w-full p-5 bg-[#001E06] mt-[10%]  h-full">
-          <div className="flex flex-col h-full md:h-[15rem]  items-center md:flex-row gap-[3rem] md:gap-[2rem]  justify-center mx-auto">
+        <div className="w-full px-5  md:px-0 py-5 bg-[#001E06] mt-[10%]  h-full">
+          <div className="flex flex-col h-full md:h-[15rem] w-full md:max-w-[75%] md:min-w-[75%]  items-center md:flex-row gap-[3rem]  justify-between mx-auto">
             <div className="flex_column gap-5 ">
               <Image
                 width={140}
@@ -28,7 +28,7 @@ const Footer = () => {
                 alt="main icon"
               />
 
-              <p className="text-[#E6F5E8] text-[14px] font-[400] flex-wrap  w-full md:max-w-[70%]">
+              <p className="text-[#E6F5E8] text-[14px] font-[400] flex-wrap  w-full md:max-w-[70%] ">
                 Maximize your earnings and unlock financial growth. Make the
                 most of your money and watch your wealth soar!
               </p>
@@ -50,46 +50,51 @@ const Footer = () => {
                 ))}
               </div>
             </div>
-            <div className="flex_column w-full md:w-[30%]  gap-5">
-              <p className="font-[500] text-[16px] text-white">Get The App</p>
-
-              <div className="flex gap-3">
-                <button className="flex gap-1 min-w-[80px]  md:my-0 items-center bg-white py-2 rounded-md px-4 justify-center">
-                  <Image
-                    width={25}
-                    height={25}
-                    src="/dapp/d-2.svg"
-                    alt="apple"
-                  />
-                  <span className="flex flex-col items-start gap-0">
-                    <p className="text-black text-[10px] md:text-[12px] ">
-                      Download on the
-                    </p>
-                    <p className="text-black text-[15px]  md:text-[20px] ">
-                      App Store
-                    </p>
-                  </span>
-                </button>
-                <button className="flex gap-1 min-w-[80px]  md:my-0 items-center bg-white py-2 rounded-md px-4 justify-center">
-                  <Image
-                    width={25}
-                    height={25}
-                    src="/dapp/d-3.svg"
-                    alt="playstore"
-                  />
-                  <span className="flex flex-col items-start gap-0">
-                    <p className="text-black text-[10px]  md:text-[12px] ">
-                      Get it on
-                    </p>
-                    <p className="text-black text-[15px]  md:text-[20px] ">
-                      Google Play
-                    </p>
-                  </span>
-                </button>
+            <div className="flex-col  w-full md:w-[40%] gap-8 ">
+              <div className="flex-col flex   w-full justify-end gap-5 ">
+                <div className=" flex md:justify-end ">
+                  <p className="font-[500] text-[16px] text-white ">
+                    Get The App
+                  </p>
+                </div>
+                <div className="flex md:flex-col md:gap-4  gap-[1rem] ">
+                  <button className="flex gap-1 md:ml-auto md:min-w-[180px] max-w-[180px]  md:my-0 items-center bg-white py-2 rounded-md px-4 justify-center">
+                    <Image
+                      width={25}
+                      height={25}
+                      src="/dapp/d-2.svg"
+                      alt="apple"
+                    />
+                    <span className="flex flex-col items-start gap-0">
+                      <p className="text-black text-[10px] md:text-[12px] ">
+                        Download on the
+                      </p>
+                      <p className="text-black text-[15px]  md:text-[20px] ">
+                        App Store
+                      </p>
+                    </span>
+                  </button>
+                  <button className="flex gap-1 md:ml-auto   md:my-0 items-center bg-white py-2 rounded-md px-4 justify-center">
+                    <Image
+                      width={25}
+                      height={25}
+                      src="/dapp/d-3.svg"
+                      alt="playstore"
+                    />
+                    <span className="flex flex-col items-start gap-0">
+                      <p className="text-black text-[10px]  md:text-[12px] ">
+                        Get it on
+                      </p>
+                      <p className="text-black text-[15px]  md:text-[20px] ">
+                        Google Play
+                      </p>
+                    </span>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-full md:w-[83%] mt-10 mx-auto gap-[3rem] md:gap-[2rem] ">
+          <div className="flex flex-col w-full md:w-[75%] mt-10 mx-auto gap-[3rem] md:gap-[2rem] ">
             <Divider
               sx={{ color: "white", background: "white", width: "100%" }}
             />
